cbc330ed6529489efc878ff7b42af7f759c66434
Hinerk Stender <hinerk.stender@mail.de>
2025-11-04 14:13:22 +0100

    improve structure of native exceptions

--- src/the_test_framework/core/exceptions.py
+++ src/the_test_framework/core/exceptions.py
@@ -1,9 +1,18 @@
-class QuitTestSystem(Exception): ...
+class TestSystemException(Exception):
+    """TestSystem base exception"""
 
-class FailedTest(Exception): ...
+class TestSystemIntrospectionError(TestSystemException):
+    """issues occuring during initial introspection"""
 
-class AbortTestSequence(Exception):
+class NoTestSystemInstanceFound(TestSystemException):
+    """raised by TestSystem.get_active_instance() if no such thing was found"""
+
+class TestSystemBaseFlowControlException(TestSystemException):
+    """TestSystem base exception for "controlling" purposes"""
+
+class QuitTestSystem(TestSystemBaseFlowControlException): ...
+
+class FailedTest(TestSystemBaseFlowControlException): ...
+
+class AbortTestSequence(TestSystemBaseFlowControlException):
     """raised by test_step to indicate the termination of the test sequence"""
-
-class NoTestSystemInstanceFound(Exception):
-    """raised by get_active_instance() to indicate that no test system instance was found"""
