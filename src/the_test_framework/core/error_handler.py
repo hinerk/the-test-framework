b32c976c2eccea28178241ce5a6facb06db184ff
Hinerk Stender <hinerk.stender@mail.de>
2026-02-18 15:53:08 +0100

    log exception in default error handler

--- src/the_test_framework/core/error_handler.py
+++ src/the_test_framework/core/error_handler.py
@@ -1,10 +1,12 @@
 import tkinter as tk
 import traceback
 from tkinter import messagebox
+from logging import getLogger
 
 
 def error_handler(exc: BaseException):
     tb_str = "".join(traceback.format_exception(type(exc), exc, exc.__traceback__))
+    getLogger(__name__).exception("error handler handles", exc_info=exc)
     root = tk.Tk()
     root.withdraw()
     root.lift()
